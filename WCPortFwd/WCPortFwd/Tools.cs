b93d31bdb5d8dff1c1cfeaefe8fec30a96d3d81c
stackprobe <stackprobes@gmail.com>
2020-02-17 03:38:43 +0900

    Ange

--- WCPortFwd/WCPortFwd/Tools.cs
+++ WCPortFwd/WCPortFwd/Tools.cs
@@ -9,39 +9,43 @@ using System.Security.Cryptography;
 
 namespace WCPortFwd
 {
+	// sync > @ UISuspend
+
 	public class UISuspend : IDisposable
 	{
 		[DllImport("user32.dll", EntryPoint = "SendMessageA")]
 		private static extern uint SendMessage(IntPtr hWnd, uint wMsg, uint wParam, uint lParam);
 
-		private const Int32 WM_SETREDRAW = 0x000B;
+		private const uint WM_SETREDRAW = 0x000B;
 
 		private static void SetWindowRedraw(IWin32Window window, bool fRedraw)
 		{
 			if ((window != null) && (window.Handle != IntPtr.Zero))
 			{
-				SendMessage(window.Handle, (uint)WM_SETREDRAW, (fRedraw) ? 1u : 0u, 0u);
+				SendMessage(window.Handle, WM_SETREDRAW, (fRedraw) ? 1u : 0u, 0u);
 			}
 		}
 
-		private Control Ctrl;
+		private Control _ctrl;
 
 		public UISuspend(Control ctrl)
 		{
 			SetWindowRedraw(ctrl, false);
-			this.Ctrl = ctrl;
+			_ctrl = ctrl;
 		}
 
 		public void Dispose()
 		{
-			if (this.Ctrl != null)
+			if (_ctrl != null)
 			{
-				SetWindowRedraw(this.Ctrl, true);
-				this.Ctrl.Refresh();
-				this.Ctrl = null;
+				SetWindowRedraw(_ctrl, true);
+				_ctrl.Refresh();
+				_ctrl = null;
 			}
 		}
 	}
+
+	// < sync
 
 	public class Counter
 	{
